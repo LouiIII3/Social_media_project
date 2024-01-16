676a475d3ec393f05d87f6df5564c69e0b222fec
LouiIII3 <119919129+LouiIII3@users.noreply.github.com>
2024-01-16 13:32:05 +0900

    Add files via upload

---
+++ Server/media/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.social.media;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findByUserid(String userid);
+    User findBykey1(String key1);
+
+    //userid가 이미 데이터베이스에 존재하는지
+    boolean existsByUserid(String userid);
+}
