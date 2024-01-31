e00be1ee9336aa75c780923b209670be61002e58
LouiIII3 <119919129+LouiIII3@users.noreply.github.com>
2024-01-31 12:18:36 +0900

    Update ArticleRepository.java

--- Server/conrtoller/ArticleRepository.java
+++ Server/conrtoller/ArticleRepository.java
@@ -4,5 +4,5 @@ import com.example.social.controller.Article;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ArticleRepository extends CrudRepository<Article, Long> {
-
+  //추후 추가 예정
 }
