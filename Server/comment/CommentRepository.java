9ec95ce1da5e57740a104742c9e487278d53c837
LouiIII3 <119919129+LouiIII3@users.noreply.github.com>
2024-02-14 12:56:21 +0900

    Add files via upload

---
+++ Server/comment/CommentRepository.java
@@ -0,0 +1,9 @@
+package com.example.social.comment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByPostId(Long postId);
+}
