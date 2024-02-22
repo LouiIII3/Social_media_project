cb310d300250218f9b1332070fe6fcf8b57d217b
LouiIII3 <119919129+LouiIII3@users.noreply.github.com>
2024-02-22 13:43:29 +0900

    Update PostRepository.java

--- Server/post/PostRepository.java
+++ Server/post/PostRepository.java
@@ -1,7 +1,6 @@
 package com.example.social.post;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
