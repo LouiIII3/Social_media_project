d7cd502d2cde99698dd27d6235e8460424f32f3e
LouiIII3 <119919129+LouiIII3@users.noreply.github.com>
2024-03-04 11:41:24 +0900

    Delete Server/conrtoller directory

--- Server/conrtoller/ArticleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.social.controller;
-
-import com.example.social.controller.Article;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ArticleRepository extends CrudRepository<Article, Long> {
-  //추후 추가 예정
-}
